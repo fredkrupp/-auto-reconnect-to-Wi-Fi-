a2e07c5d5d81f525165ac02538a7f8cf21cf5d42
fredkrupp <a1fredkrupp7@gmail.com>
2024-05-12 06:47:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import subprocess
 import time
+from pyrogram import Client
 import platform
-print("activate")
+api_id = 
+api_hash = 
 
+app = Client("my_account", api_id=api_id, api_hash=api_hash)
+app.start()
+print("activate")
 def myping(host):
     parameter = "-n" if platform.system().lower() == "windows" else "-c"
     command = ["ping", parameter, "1", host]
     response = subprocess.call(command)
     return response == 0
+
+
 while True:
-    if myping("www.google.com")<=7:
-        cmd1 = 'netsh wlan disconnect'
-        cmd2 = 'netsh wlan connect name="TP-Link_6FBC"'
-        if 'netsh wlan disconnect':
-            subprocess.call(cmd2, shell=True)
-        else:
-            print("Отключение...")
-            subprocess.call(cmd1, shell=True)
-            time.sleep(2)
-            subprocess.call(cmd2, shell=True)
-    else:
+    if myping("store.steampowered.com")<10:
+        app.send_message(1052872948,"живу")
         pass
+
+    else:
+        if 'netsh wlan disconnect':
+            subprocess.call('netsh wlan connect name="TP-Link_6FBC"', shell=True)
+        else:
+            app.send_message(1052872948,"умер")
+            print("Отключение...")
+            subprocess.call('netsh wlan disconnect', shell=True)
+            time.sleep(2)
+            subprocess.call('netsh wlan connect name="TP-Link_6FBC"', shell=True)
+
     time.sleep(5)
