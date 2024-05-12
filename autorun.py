8269ffb1aef0af3cd1b48e4e96ad5f963f7852f0
fredkrupp <a1fredkrupp7@gmail.com>
2024-05-12 04:39:32 +0300

    Update autorun.py

--- autorun.py
+++ autorun.py
@@ -2,28 +2,20 @@ import os
 import getpass
 from pathlib import Path
 
+USER_NAME = getpass.getuser()
 
 def add_to_startup(file_path=None):
-
     if not file_path:
-        file_path = f'{Path.home()}main.py'
+        file_path = r"C:\Users\PC\main.py"
     
-
-    startup_folder = Path(os.environ['APPDATA']).joinpath('Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup').as_posix()
-    
-
-    bat_file_path = f"{startup_folder}\\open2.bat"
+    bat_path = os.path.join(os.environ['APPDATA'], 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup')
+    bat_file_path = os.path.join(bat_path, 'open.bat')
     
     try:
-
         with open(bat_file_path, 'w+') as bat_file:
             bat_file.write(f"python {file_path}")
-        
-
         print("Файл успешно добавлен в автозагрузку.")
     except Exception as e:
-
-        print(f"Произошла ошибка при добавлении файла в автозагрузку: {e}")
-
+        print("Произошла ошибка при добавлении файла в автозагрузку: ", e)
 
 add_to_startup()
