4e3d14ab4c1b89baa3e1bfcebdabeef71a862a6f
fredkrupp <a1fredkrupp7@gmail.com>
2024-05-12 04:39:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import subprocess
 import time
+print("activate")
 def myping(host):
-    command = ["ping", "-n", "1", host]  # Используйте прямые строковые литералы для команды
+    command = ["ping", "-n", "1", host]
     return subprocess.call(command) == 0
 
 while True:
-    if myping("www.google.com")<=0:
-        print("Неудача при пинге «www.google.com». Отключение...")
-    # Убедитесь, что ваши команды netsh работают правильно
+    if myping("www.google.com")<=7:
         cmd1 = 'netsh wlan disconnect'
-        subprocess.call(cmd1, shell=True)
-        time.sleep(2)  # Используйте time.sleep вместо ts
         cmd2 = 'netsh wlan connect name="TP-Link_6FBC"'
-        subprocess.call(cmd2, shell=True)
+        if 'netsh wlan disconnect':
+            subprocess.call(cmd2, shell=True)
+        else:
+            print("Отключение...")
+            subprocess.call(cmd1, shell=True)
+            time.sleep(2)
+            subprocess.call(cmd2, shell=True)
     else:
         pass
+    time.sleep(5)
